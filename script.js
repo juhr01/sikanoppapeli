330ce951e83cd7169eb4701972c3e4c13b037714
juhr01 <juho1.riipinen@gmail.com>
2022-08-24 13:13:47 +0300

    hienosäätöä vailla

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ let pelivuoro = 1;
 
 let pisteet1 = 0;
 let pisteet2 = 0;
-let pisteet3 = 0; 
+let pisteet3 = 0;
 
-let pelaaja1 = "Pelaaja 1";
-let pelaaja2 = "Pelaaja 2";
-let pelaaja3 = "Pelaaja 3";
+let pelaaja1;
+let pelaaja2;
+let pelaaja3;
 
 let noppa1 = 0;
-let noppa2 = 0
+let noppa2 = 0;
 
 let nopat0 = new Image();
 nopat0.src = "kuvat/noppa1.png"
@@ -102,12 +102,14 @@ function kaksiPelaajaa() {
         pelaaja1 = "Pelaaja 1";
     } else {
         pelaaja1 = document.getElementById("pelaaja1nimi2").value;
+        document.getElementById("pelaaja1").innerHTML = pelaaja1;
     }
 
     if (document.getElementById("pelaaja2nimi2").value.trim().length == 0) {
         pelaaja2 = "Pelaaja 2";
     } else {
         pelaaja2 = document.getElementById("pelaaja2nimi2").value;
+        document.getElementById("pelaaja2").innerHTML = pelaaja2;
     }
 
     pelaajienMaara = 2;
@@ -141,77 +143,84 @@ function kolmePelaajaa() {
     document.getElementById("pelaaja1t").style.display = "block";
     document.getElementById("pelaaja2t").style.display = "block";
     document.getElementById("pelaaja3t").style.display = "block";
-    
+
 }
 
-function arvoNopat() {
+function arvoNoppa1() {
     noppa1 = Math.round(Math.random() * 5)
-    if (noppienMaara == 2) {
-        noppa2 = Math.round(Math.random() * 5)
-    }
-
     document.getElementById("noppa1").src = eval("nopat" + noppa1 + ".src");
-    document.getElementById("noppa2").src = eval("nopat" + noppa2 + ".src");
+    return noppa1;
 
+}
+
+function arvoNoppa2() {
+    noppa2 = Math.round(Math.random() * 5)
+    document.getElementById("noppa2").src = eval("nopat" + noppa2 + ".src");
+    return noppa2;
 }
 
 
 
 function pelaa() {
+    document.getElementById("pelivuoro").innerHTML = pelaaja1;
     document.getElementById("valikko").style.display = "none";
     document.getElementById("peli").style.display = "block";
 
 }
 
 function heita() {
-    arvoNopat();
+    arvoNoppa1();
+    if (noppienMaara == 2) {
+        arvoNoppa2();
+    }
     arvioiTulos();
 }
 
 function arvioiTulos() {
-    if (noppienMaara == 2 && tuplat == 3) {
+    if (noppienMaara == 2 && tuplat == 2 && noppa1 == noppa2) {
         pisteetNyt = 0;
         lopetaVuoro();
         return;
     }
 
-    if (noppienMaara == 1) {
-        if (noppa1 = 0) {
-            pisteetNyt = 0;
-            lopetaVuoro();
-            return;
-        }
+    if (noppienMaara == 2 && ((noppa1 == 0 && noppa2 != 0) || (noppa1 != 0 && noppa2 == 0))) {
+        pisteetNyt = 0;
+        lopetaVuoro();
+        return;
+    }
 
-    } else {
-        if ((noppa1 == 0 && noppa2 != 0) || (noppa1 != 0 && noppa2 == 0)) {
-            pisteetNyt = 0;
-            lopetaVuoro();
-            return;
-        }
+    if (noppienMaara == 1 && noppa1 == 0) {
+        pisteetNyt = 0;
+        lopetaVuoro();
+        return;
+
     }
 
 
+
+
     if (noppienMaara == 1) {
-        pisteetNyt = 1 + noppa1 + pisteetNyt;
-        document.getElementById("pisteetNyt").innerHTML = pisteetNyt;
+        pisteetNyt = pisteetNyt + 1 + noppa1;
+        document.getElementById("pisteet").innerHTML = pisteetNyt;
         return;
     } else {
         if ((noppa1 == 0 && noppa2 == 0) && tuplat <= 2) {
             pisteetNyt = pisteetNyt + 25;
             tuplat = tuplat + 1;
-            document.getElementById("pisteetNyt").innerHTML = pisteetNyt;
-            document.getElementById("tupliaHeitetty").innerHTML = tuplat;
+            document.getElementById("pisteet").innerHTML = pisteetNyt;
+            document.getElementById("tuplat").innerHTML = tuplat;
             return;
-        
-        } else if (noppa1 == noppa2 && noppa1 != 0 && tuplat <=2) {
+
+        } else if (noppa1 == noppa2 && noppa1 != 0 && tuplat <= 2) {
             pisteetNyt = (noppa1 + noppa2 + 2) * 2 + pisteetNyt;
             tuplat = tuplat + 1;
-            document.getElementById("pisteetNyt").innerHTML = pisteetNyt;
-            document.getElementById("tupliaHeitetty").innerHTML = tuplat;
+            document.getElementById("pisteet").innerHTML = pisteetNyt;
+            document.getElementById("tuplat").innerHTML = tuplat;
             return;
         } else {
             pisteetNyt = noppa1 + noppa2 + 2 + pisteetNyt;
-            document.getElementById("pisteetNyt").innerHTML = pisteetNyt;
+            document.getElementById("pisteet").innerHTML = pisteetNyt;
+            return;
         }
     }
 }
@@ -219,74 +228,103 @@ function arvioiTulos() {
 
 function lopetaVuoro() {
     if (pisteet1 + pisteetNyt >= 100 && pelivuoro == 1) {
-        peliLoppuu();
         document.getElementById("peli").style.display = "none";
         document.getElementById("voittaja").innerHTML = pelaaja1;
         document.getElementById("peliloppu").style.display = "block";
-    }
-    if (pisteet2 + pisteetNyt >= 100 && pelivuoro == 2) {
-        peliLoppuu();
+    } else if (pisteet2 + pisteetNyt >= 100 && pelivuoro == 2) {
         document.getElementById("peli").style.display = "none";
         document.getElementById("voittaja").innerHTML = pelaaja2;
         document.getElementById("peliloppu").style.display = "block";
-    }
-    if (pisteet3 + pisteetNyt >= 100 && pelivuoro == 3) {
-        peliLoppuu();
+    } else if (pisteet3 + pisteetNyt >= 100 && pelivuoro == 3) {
         document.getElementById("peli").style.display = "none";
         document.getElementById("voittaja").innerHTML = pelaaja3;
         document.getElementById("peliloppu").style.display = "block";
-    }
-
-    if (pelivuoro == 3 && pelaajienMaara == 3) {
-        pelivuoro = 1;
-        document.getElementById("pelivuoro").innerHTML = pelaaja1;
-        pisteet3 = pisteetNyt + pisteet3;
-        document.getElementById("pelaaja3p").innerHTML = pisteet3;
-        vaihdaVuoroa();
-        return;
-    }
-
-    if (pelivuoro == 2 && pelaajienMaara == 2) {
-        pelivuoro = 1;
-        document.getElementById("pelivuoro").innerHTML = pelaaja1;
-        pisteet2 = pisteetNyt + pisteet2;
-        document.getElementById("pelaaja2p").innerHTML = pisteet2;
-        vaihdaVuoroa();
-        return;
-    }
-
-    if (pelivuoro == 2 && pelaajienMaara == 3) {
-        pelivuoro = pelivuoro + 1;
-        document.getElementById("pelivuoro").innerHTML = pelaaja3;
-        pisteet2 = pisteetNyt + pisteet2;
-        document.getElementById("pelaaja2p").innerHTML = pisteet2;
-        vaihdaVuoroa();
-        return;
-    }
-   
-    if (pelivuoro == 1) {
-        pelivuoro == pelivuoro + 1;
-        document.getElementById("pelivuoro").innerHTML = pelaaja2;
-        pisteet1 = pisteet1 + pisteetNyt;
-        document.getElementById("pelaaja1p").innerHTML = pisteet1;
-        vaihdaVuoroa();
-        return;
+    } else {
+        jatkaEsiin();
     }
 
 
 
-}
 
-function peliLoppuu() {
-    
+
 }
 
 function vaihdaVuoroa() {
+    if (pelivuoro == 3 && pelaajienMaara == 3) {
+        pelivuoro = 1;
+        document.getElementById("pelivuoro").innerHTML = pelaaja1;
+        return;
+    }
+    if (pelivuoro == 2 && pelaajienMaara == 2) {
+        pelivuoro = 1;
+        document.getElementById("pelivuoro").innerHTML = pelaaja1;
+        return;
+    }
+    if (pelivuoro == 2) {
+        pelivuoro = pelivuoro + 1;
+        document.getElementById("pelivuoro").innerHTML = pelaaja3;
+        return;
+    }
+    if (pelivuoro == 1) {
+        pelivuoro = pelivuoro + 1;
+        document.getElementById("pelivuoro").innerHTML = pelaaja2;
+        return;
+    }
+
+}
+
+
+function jatkaEsiin() {
+    document.getElementById("jatka").style.display = "block";
+    document.getElementById("vuoroohi").style.display = "block";
+    document.getElementById("tupliaHeitetty").style.display = "none";
+    document.getElementById("pisteetNyt").style.display = "none";
+    document.getElementById("heita").style.display = "none";
+    document.getElementById("lopeta").style.display = "none";
+
+    if (pelivuoro == 3 && pelaajienMaara == 3) {
+        pisteet3 = pisteetNyt + pisteet3;
+        document.getElementById("pelaaja3p").innerHTML = pisteet3;
+        return;
+    }
+    if (pelivuoro == 2 && pelaajienMaara == 2) {
+        pisteet2 = pisteetNyt + pisteet2;
+        document.getElementById("pelaaja2p").innerHTML = pisteet2;
+        return;
+    }
+    if (pelivuoro == 2) {
+        pisteet2 = pisteet2 + pisteetNyt;
+        document.getElementById("pelaaja2p").innerHTML = pisteet2;
+        return;
+    }
+    if (pelivuoro == 1) {
+        pisteet1 = pisteet1 + pisteetNyt;
+        document.getElementById("pelaaja1p").innerHTML = pisteet1;
+        return;
+    }
+}
+
+function jatka() {
+    vaihdaVuoroa();
+    nollaaPisteet();
+    document.getElementById("jatka").style.display = "none";
+    document.getElementById("vuoroohi").style.display = "none";
+
+    if (noppienMaara == 2) {
+        document.getElementById("tupliaHeitetty").style.display = "block";
+    }
+
+    document.getElementById("pisteetNyt").style.display = "block";
+    document.getElementById("heita").style.display = "block";
+    document.getElementById("lopeta").style.display = "block";
+}
+
+function nollaaPisteet() {
     pisteetNyt = 0;
     tuplat = 0;
 
     document.getElementById("noppa1").src = "kuvat/tyhjanoppa.png";
     document.getElementById("noppa2").src = "kuvat/tyhjanoppa.png";
-    document.getElementById("tupliaHeitetty").innerHTML = tuplat; 
+    document.getElementById("tuplat").innerHTML = tuplat;
+    document.getElementById("pisteet").innerHTML = pisteetNyt;
 }
-
